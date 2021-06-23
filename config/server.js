25714579455b29788e7ca1f64c3ca11ed8659f9a
Sarkan Aliyev <sarkanaliyev@gmail.com>
2021-06-23 18:03:52 +0400

    third commit

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "10.2.214.2"),
+  port: env.int("PORT", 80),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '8437314f82ba7173d8f3e96a7ec6d492'),
+      secret: env("ADMIN_JWT_SECRET", "8437314f82ba7173d8f3e96a7ec6d492"),
     },
   },
 });
